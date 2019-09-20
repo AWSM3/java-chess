58be031c513b063cc933c96b38391af0f705157f
Dmitry <dmitry2500u@bk.ru>
2019-09-20 14:30:24 +0300

    Разделение по профилям, использование переменных и скоупов

--- run.bat
+++ run.bat
@@ -1,1 +1,4 @@
-mvn clean compile assembly:single
+:: запуск дефолтного профиля, можно без -Pdefault, так как у этого профиля стоит activeByDefault=true
+mvn -Pdefault clean compile assembly:single 
+:: запуск профиля oversize
+mvn -Poversize clean compile assembly:single
---
+++ src/main/java/com/lanit/chess/AppOversize.java
@@ -0,0 +1,19 @@
+package com.lanit.chess;
+
+import com.lanit.chess.board.Board;
+import com.lanit.chess.player.Player;
+import com.lanit.chess.Color;
+
+/**
+ * - не учитывается ход пешки из начальной позиции на две клетки
+ * - превращение пешки не происходит
+ */
+public class AppOversize {
+	public static final int BOARD_SIZE = 10;
+
+	public static void main(String[] args) {
+		Board board = new Board(BOARD_SIZE);
+		
+		new Gamer(board, new Player(Color.WHITE), new Player(Color.BLACK)).run();
+	}
+}
